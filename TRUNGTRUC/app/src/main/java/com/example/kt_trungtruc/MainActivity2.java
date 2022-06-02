90a899b7f298291f0d61280e12fa2b178baeb89b
Truc0308191091 <94152322+Truc0308191091@users.noreply.github.com>
2022-06-02 09:41:54 +0700

    KT2_TRUNGTRUC

---
+++ TRUNGTRUC/app/src/main/java/com/example/kt_trungtruc/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.kt_trungtruc;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView canhan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("bai1");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
---
+++ TRUNGTRUC/app/src/main/java/com/example/kt_trungtruc/MainActivity3.java
@@ -0,0 +1,36 @@
+package com.example.kt_trungtruc;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity3 extends AppCompatActivity {
+    TextView canhan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("bài 2");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
